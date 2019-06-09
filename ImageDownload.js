9bde53d60f33ae0bdd2bdd8f96d0e63ac5dececf
sonsuke <bay0211@icloud.com>
2019-06-09 22:53:05 +0900

    取得・5分間隔

--- ImageDownload.js
+++ ImageDownload.js
@@ -1,7 +1,18 @@
 
 require('date-utils');
 var nowTime = new Date();
-var format = nowTime.toFormat("YYYYMMDDHH2430");
+var minutes = nowTime.getMinutes();
+var ArrayMinutes = String(minutes).split('');
+console.log(ArrayMinutes);
+
+if(ArrayMinutes[1] % 5 == 0){
+
+}else if(0<ArrayMinutes[1]<5){
+  ArrayMinutes[1] = 0;
+}else{
+  ArrayMinutes[1] = 5;
+}
+var format = nowTime.toFormat("YYYYMMDDHH24"+ArrayMinutes[0]+ArrayMinutes[1]);
 console.log(format);
 
 var req = require('request');
@@ -11,8 +22,7 @@ req(
   {method: 'GET', url: "http://www.jma.go.jp/jp/radnowc/imgs/radar/205/"+format+"-00.png", encoding: null},
   function (error, response, body){
     if(!error && response.statusCode === 200){
-      fs.writeFileSync('a.png', body, 'binary');
-      console.log(i);
+      fs.writeFileSync(format+'.png', body, 'binary');
     }
   }
 );
