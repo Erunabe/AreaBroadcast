f29d6192049b6735672d1e8c9bc6e494e2912793
sonsuke <bay0211@icloud.com>
2019-07-19 23:34:10 +0900

    定期実行確認

--- DataInsertX.js
+++ DataInsertX.js
@@ -10,7 +10,7 @@ var options = {
     url: 'http://api.potekanet.com/v1/point/real/ja/poteka?potekaId=555&element=temp,humi,wind_s,wind_d,wind_max_s,press_l,rain_i,rain_m,wbgt',
     method: 'GET',
     headers: headers,
-    proxy:'http://s1500740:Rose-lisa910@wproxy.net.sendai-nct.ac.jp:8080',
+    //proxy:'http://s1500740:Rose-lisa910@wproxy.net.sendai-nct.ac.jp:8080',
     json: false,
 }
 
@@ -21,11 +21,29 @@ request(options, function (error, response, body) {
     console.log(body);
     res = JSON.parse(body);
 
+
     datatime = res.poteka[0].element[0].dataList[0].datatime;
     temp = res.poteka[0].element[0].dataList[0].value;
     humi = res.poteka[0].element[1].dataList[0].value;
     wind_s = res.poteka[0].element[2].dataList[0].value;
     wind_d = res.poteka[0].element[3].dataList[0].value;
+    if(0<=wind_d && wind_d<=22.5 && 337.5 < wind_d && wind_d<=360){
+      wind_d = "北";
+    }else if (22.5<wind_d && wind_d<=67.5) {
+      wind_d = "北東";
+    }else if (67.5<wind_d && wind_d<=112.5) {
+      wind_d = "東";
+    }else if (112.5<wind_d && wind_d<=157.5) {
+      wind_d = "南東";
+    }else if (157.5<wind_d && wind_d<=202.5) {
+      wind_d = "南";
+    }else if (202.5<wind_d && wind_d<=247.5) {
+      wind_d = "南西";
+    }else if (247.5<wind_d && wind_d<=292.5) {
+      wind_d = "西";
+    }else if (292.5<wind_d && wind_d<=337.5) {
+      wind_d = "北西";
+    }
     wind_max_s = res.poteka[0].element[4].dataList[0].value;
     press_l = res.poteka[0].element[5].dataList[0].value;
     rain_i = res.poteka[0].element[6].dataList[0].value;
