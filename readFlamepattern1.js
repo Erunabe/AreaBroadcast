3c27881e17fae15c431dbf1da6e900fea2390a4e
sonsuke <bay0211@icloud.com>
2019-10-30 18:37:04 +0900

    10/30

--- readFlamepattern1.js
+++ readFlamepattern1.js
@@ -9,17 +9,18 @@ var srv = http.createServer();
 var temp = fs.readFileSync("./flamepattern1.ejs", "utf-8");
 srv.on("request", function(req, res) {
 var page = ejs.render(temp, {
-datatime:readData.datatime,
-temp:readData.temp,
-humi:readData.humi,
-wind_s:readData.wind_s,
-wind_d:readData.wind_d,
-wind_max_s:readData.wind_max_s,
-press_l:readData.press_l,
-rain_i:readData.rain_i,
-rain_m:readData.rain_m,
-wbgt:readData.wbgt,
+  datatime:readData.datatime,
+  temp:readData.temp,
+  humi:readData.humi,
+  wind_s:readData.wind_s,
+  wind_d:readData.wind_d,
+  wind_max_s:readData.wind_max_s,
+  press_l:readData.press_l,
+  rain_i:readData.rain_i,
+  rain_m:readData.rain_m,
+  wbgt:readData.wbgt,
 });
+
 res.writeHead(200, {"Content-Type": "text/html"});
 res.write(page);
 res.end();
