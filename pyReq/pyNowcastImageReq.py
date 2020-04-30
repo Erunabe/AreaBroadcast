43fd5e6b09dcf7ad826d1c88feaee82834fbbe38
sonsuke <bay0211@icloud.com>
2020-04-30 23:51:52 +0900

    4/30

---
+++ pyReq/pyNowcastImageReq.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+#!/usr/bin/python　3
+
+
+import requests
+import json
+import datetime
+import os
+
+#os.environ["http_proxy"] = "http://10.64.199.79:8080"
+now = datetime.datetime.now()
+
+nowTime = now.strftime('%Y%m%d%H')
+minutes=now.strftime('%M')
+splitMinutes=list(minutes)
+endMinutes = int(splitMinutes[1])
+
+
+if endMinutes % 5 == 0:
+    pass
+elif 0<endMinutes<5:
+    endMinutes=0;
+else:
+    endMinutes=5;
+
+print(endMinutes)
+
+format1 = nowTime+splitMinutes[0]+str(endMinutes)+'-00.png'
+format2 = nowTime+splitMinutes[0]+str(endMinutes)+'.png'
+
+
+URL = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/205/'+format1
+requestImage = requests.get(URL)
+
+
+with open('/home/a2011529/AreaBroadcast/pyReq/pyNowcastImage/'+format2,'wb') as f:
+ f.write(requestImage.content)
---
+++ pyReq/pyWeathermapImageReq.py
@@ -0,0 +1,46 @@
+# coding:utf-8
+#!/usr/bin/python　3
+
+
+import requests
+import json
+import datetime
+import os
+
+#os.environ["http_proxy"] = "http://10.64.199.79:8080"
+now = datetime.datetime.now()
+
+year = now.strftime('%Y')
+splitYear=list(year)
+MD=now.strftime('%m%d')
+timeFormat=int(now.strftime('%H%M'))
+hourList = [3,6,9,12,15,18,21]
+
+
+if 510<= timeFormat <810:
+  hourFormat = "0"+ str(hourList[0]);
+elif 810<= timeFormat <1110:
+  hourFormat = "0"+ str(hourList[1]);
+elif 1110<= timeFormat <1410:
+  hourFormat = "0"+ str(hourList[2]);
+elif 1410<= timeFormat <1710:
+  hourFormat = str(hourList[3]);
+elif 1710<= timeFormat <2010:
+  hourFormat = str(hourList[4]);
+elif 2010<= timeFormat <2310:
+  hourFormat = str(hourList[5]);
+else:
+  hourFormat = str(hourList[6]);
+
+print(hourFormat)
+
+format = splitYear[2]+splitYear[3]+MD+hourFormat+'.png'
+
+
+
+URL = 'https://www.jma.go.jp/jp/g3/images/jp_c/'+format
+requestImage = requests.get(URL)
+
+
+with open('/home/a2011529/AreaBroadcast/pyReq/pyWeathermapImage/'+format,'wb') as f:
+ f.write(requestImage.content)
